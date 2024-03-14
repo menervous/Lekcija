cf2b91159a72d6fc9d3ec864246a2d3c32798d3e
menervous <znotinskriss@gmail.com>
2024-03-14 14:11:26 +0200

    pirmais

---
+++ Assets/Scripts/playerHP.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHP : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public Text healthText;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+        UpdateHealthUI();
+    }
+
+    void Die()
+    {
+        Debug.Log("Player has died");
+    }
+
+    void UpdateHealthUI()
+    {
+        if (healthText != null)
+        {
+            healthText.text = "Health: " + currentHealth.ToString();
+        }
+    }
+}
